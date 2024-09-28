36a7ba9a32e27f44edcf5edcbed1a83ec1476a20
talles-shipay <tallesmonteiro0@gmail.com>
2024-09-28 13:18:20 -0300

    Cria meu grupo de segurança

---
+++ terraform/security-groups.tf
@@ -0,0 +1,31 @@
+# Cria meu grupo de segurança, liberando porta ssh e http para entrada de toda internet, e libera toda saída para internet.
+resource "aws_security_group" "wordpress_sg" {
+  vpc_id = aws_vpc.wordpress_turbinado.id
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "wordpress-sg"
+  }
+}
